1fa426dd46ce3751fc0b5a6806f233a7efde4602
Blake Yoder <yoderblake@gmail.com>
2018-11-15 11:55:45 -0500

    Automate creating an account

--- __tests__/test.login.js
+++ __tests__/test.login.js
@@ -1,18 +1,25 @@
 const puppeteer = require('puppeteer');
+const crypto = require('crypto');
 
 describe('Lendly e2e loan flow', () => {
   beforeAll(async () => {
-    await page.goto('https://getlendly.com', {waitUntil: 'load'});
-    await page.setViewport({width: 0, height: 0});
+    await page.goto('https://lendly.fk', {waitUntil: 'load'});
+    // hack to get dom elements to appear in headless mode
+    await page.setViewport({width: 2000, height: 1200});
+    console.log('Opening up `lendly.fk`...');
   });
 
-  it('user can sign up for a loan', async () => {
+  it('user can sign up for a loan', async done => {
     await Promise.all([
       page.click('.site-nav__list > li:nth-child(7)'),
       page.waitForNavigation({waitUntil: 'load'}),
     ]);
+    
+    console.log('Cicking `Get Started`...');
 
     await page.select('#piYsState', 'IN');
+
+    console.log('Selecting Indiana from dropdown...');
 
     await Promise.all([
       page.click('button'),
@@ -20,25 +27,49 @@ describe('Lendly e2e loan flow', () => {
     ]);
 
     await page.click('#inEmprName');
-    await page.type('#inEmprName', 'wal mart', {delay: 120});
+    await page.type('#inEmprName', 'wal ma', {delay: 80});
+    console.log('Using `wal mart` as employer name...');
     await page.keyboard.down('ArrowDown');
+    console.log('Arrow down...');
     await page.click('button');
-    await page.select('#start_month', '10');
+    console.log('Submit employeer info...');
+    await page.select('#start_month', '8');
     await page.select('#start_year', '2013');
+    console.log('Selecting starting dates...');
 
     await Promise.all([
       page.click('button'),
+      console.log('Navigating to next page...'),
       page.waitForNavigation({waitUntil: 'load'}),
     ]);
 
     await Promise.all([
       page.click('#isBankrupt > li:nth-child(2)'),
+      console.log('Selecting `no`. Im not declaring bankruptcy...'),
       page.waitForNavigation({waitUntil: 'load'}),
     ]);
 
     await Promise.all([
       page.click('#activeMilitaryCustomer > li:nth-child(2)'),
+      console.log('Selecting `no`. Im not military...'),
       page.waitForNavigation({waitUntil: 'load'}),
     ]);
-  }, 15000);
+
+    const usernameSuffix = crypto.randomBytes(8).toString('hex'); 
+    const username = `test-runner__${usernameSuffix}@tester.com`;
+    const password = crypto.randomBytes(16).toString('hex'); 
+
+    await page.type('#emailAddress', username, {delay: 50});
+    await page.type('#pwd', password, {delay: 50});
+    console.log({username, password});
+
+    await Promise.all([
+      page.click('.btn-icon--arrow'),
+      console.log('Creating new account...'),
+      page.waitForNavigation({waitUntil: 'load'}),
+    ]);
+
+    done();
+
+  }, 500000);
 })
